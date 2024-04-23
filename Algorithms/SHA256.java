20cdac1f6cdf947eeff37be7d3c43cfa57f295be
Seth Wilfinger <130659278+WilfingerS@users.noreply.github.com>
2024-04-23 16:43:13 -0400

    uahgdafoas

--- Algorithms/SHA256.java
+++ Algorithms/SHA256.java
@@ -4,36 +4,37 @@ import java.security.SecureRandom;
 import java.util.Base64;
 
 public class SHA256 {
-        //For testing
+        /*For testing
         public static void main(String[] args) {
-        String passwordToHash = "yourPassword";
+        String unhashed = "yourPassword";
         
         // Generate a new salt
         byte[] salt = createSalt();
         
         // Hash the password with the salt
-        String securePassword = getSecurePassword(passwordToHash, salt);
+        String securePassword = getSecurePassword(unhashed, salt);
         
         // Save the salt and the hash somewhere (e.g., a database)
         System.out.println("Secure Password: " + securePassword);
         System.out.println("Salt: " + Base64.getEncoder().encodeToString(salt));
-        
+        System.out.println(securePassword.length());
+    
         // Example of saving and using them later
-        String savedSalt = Base64.getEncoder().encodeToString(salt);
+        String savedSalt = byteToStr(salt);
         String savedHash = securePassword;
         
         // Simulate retrieving and checking the hash
-        boolean isSame = verifyUserPassword("yourPassword", savedHash, Base64.getDecoder().decode(savedSalt));
+        boolean isSame = verifyUserPassword("yourPassword", savedHash, strToByte(savedSalt));
         System.out.println("Password match: " + isSame);
     }
     //*/
 
     // Hashes the password with the salt and returns the hash
-    private static String getSecurePassword(String passwordToHash, byte[] salt) {
+    private static String getSecurePassword(String unhashed, byte[] salt) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(salt);
-            byte[] bytes = md.digest(passwordToHash.getBytes());
+            byte[] bytes = md.digest(unhashed.getBytes());
             StringBuilder sb = new StringBuilder();
             for (byte aByte : bytes) {
                 sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
@@ -56,6 +57,14 @@ public class SHA256 {
             e.printStackTrace();
         }
         return salt;
+    }
+
+    public static String byteToStr(byte[] salt){
+        return Base64.getEncoder().encodeToString(salt);
+    }
+
+    public static byte[] strToByte(String str){
+        return Base64.getDecoder().decode(str);
     }
 
     // Verifies a password against a hash and salt
